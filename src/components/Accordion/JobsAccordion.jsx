1eede536f05ea1053be8fc2df92d0c9f5a5c141e
shshalam68 <a.k.mshahalam68@gmail.com>
2024-07-04 04:12:33 +0600

    sign up button fix

--- src/components/Accordion/JobsAccordion.jsx
+++ src/components/Accordion/JobsAccordion.jsx
@@ -78,95 +78,111 @@ export default function JobsAccordion() {
 
   return (
     <div>
-      {data.map((job, index) => (
-        <Accordion
-          key={index}
-          className="mb-4 mt-4"
-          sx={{
-            border: "2px solid rgba(0,0,0,.2)",
-            boxShadow: "none",
-            backgroundColor: "rgb(239, 243, 246)",
-            borderRadius: "8px",
-            "&:first-of-type": {
-              borderTopLeftRadius: "8px",
-              borderTopRightRadius: "8px",
-            },
-            "&:last-of-type": {
-              borderBottomLeftRadius: "8px",
-              borderBottomRightRadius: "8px",
-            },
-          }}
-        >
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls={`panel${index}-content`}
-            id={`panel${index}-header`}
-            className="text-xl"
+      {data.length === 0 ? (
+        <Box>
+          <Typography variant="h6">No jobs available now.</Typography>
+          <Button
+            onClick={() => {
+              // Handle create job button click action here
+              console.log("Create job button clicked");
+            }}
+            variant="outlined"
+            sx={{ mt: 2 }}
           >
-            <Typography
-              sx={{
-                color: "rgba(0, 0, 0, 0.6)",
-                fontWeight: "600",
-                fontSize: "19px",
-              }}
+            Create Job
+          </Button>
+        </Box>
+      ) : (
+        data.map((job, index) => (
+          <Accordion
+            key={index}
+            className="mb-4 mt-4"
+            sx={{
+              border: "2px solid rgba(0,0,0,.2)",
+              boxShadow: "none",
+              backgroundColor: "rgb(239, 243, 246)",
+              borderRadius: "8px",
+              "&:first-of-type": {
+                borderTopLeftRadius: "8px",
+                borderTopRightRadius: "8px",
+              },
+              "&:last-of-type": {
+                borderBottomLeftRadius: "8px",
+                borderBottomRightRadius: "8px",
+              },
+            }}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls={`panel${index}-content`}
+              id={`panel${index}-header`}
+              className="text-xl"
             >
-              {job.category} {/* Show job category */}
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            {job.roles.map((role, roleIndex) => (
-              <Box
-                key={roleIndex}
+              <Typography
                 sx={{
-                  margin: "12px 0",
-                  fontSize: "16px",
-                  backgroundColor: "#fff",
-                  padding: "10px",
-                  borderRadius: "5px",
-                  display: "flex",
-                  justifyContent: "space-between",
-                  alignItems: "center",
+                  color: "rgba(0, 0, 0, 0.6)",
+                  fontWeight: "600",
+                  fontSize: "19px",
                 }}
               >
-                <Typography>{role}</Typography> {/* Show job role */}
-                <Box>
-                  <Button
-                    onClick={() => handleClickOpen(job, role)}
-                    sx={{
-                      backgroundColor: "#fff",
-                      border: "1px solid #f5f5f5",
-                      marginRight: "15px",
-                      borderRadius: "5px",
-                      color: "#000",
-                      "&:hover": {
-                        backgroundColor: "#182f59",
-                        color: "#fff",
-                      },
-                    }}
-                  >
-                    <Typography sx={{ padding: "5px" }}>Edit Job</Typography>
-                  </Button>
-                  <Button
-                    onClick={() => handleDeleteRole(job._id, role)}
-                    sx={{
-                      backgroundColor: "#fff",
-                      border: "1px solid #f5f5f5",
-                      borderRadius: "5px",
-                      color: "#000",
-                      "&:hover": {
-                        backgroundColor: "#182f59",
-                        color: "#fff",
-                      },
-                    }}
-                  >
-                    <Typography sx={{ padding: "5px" }}>Delete Job</Typography>
-                  </Button>
+                {job.category} {/* Show job category */}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              {job.roles.map((role, roleIndex) => (
+                <Box
+                  key={roleIndex}
+                  sx={{
+                    margin: "12px 0",
+                    fontSize: "16px",
+                    backgroundColor: "#fff",
+                    padding: "10px",
+                    borderRadius: "5px",
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
+                  <Typography>{role}</Typography> {/* Show job role */}
+                  <Box>
+                    <Button
+                      onClick={() => handleClickOpen(job, role)}
+                      sx={{
+                        backgroundColor: "#fff",
+                        border: "1px solid #f5f5f5",
+                        marginRight: "15px",
+                        borderRadius: "5px",
+                        color: "#000",
+                        "&:hover": {
+                          backgroundColor: "#182f59",
+                          color: "#fff",
+                        },
+                      }}
+                    >
+                      <Typography sx={{ padding: "5px" }}>Edit Job</Typography>
+                    </Button>
+                    <Button
+                      onClick={() => handleDeleteRole(job._id, role)}
+                      sx={{
+                        backgroundColor: "#fff",
+                        border: "1px solid #f5f5f5",
+                        borderRadius: "5px",
+                        color: "#000",
+                        "&:hover": {
+                          backgroundColor: "#182f59",
+                          color: "#fff",
+                        },
+                      }}
+                    >
+                      <Typography sx={{ padding: "5px" }}>Delete Job</Typography>
+                    </Button>
+                  </Box>
                 </Box>
-              </Box>
-            ))}
-          </AccordionDetails>
-        </Accordion>
-      ))}
+              ))}
+            </AccordionDetails>
+          </Accordion>
+        ))
+      )}
 
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Edit Job Role</DialogTitle>
